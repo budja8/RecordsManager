08327bbe894c6cb59ed8b1b5e3844be30c9a214b
budja8 <dc79826@gmail.com>
2021-12-06 11:46:27 -0600

    Unit test working

--- src/main/java/edu/uaslp/project/RecordManager.java
+++ src/main/java/edu/uaslp/project/RecordManager.java
@@ -1,72 +1,55 @@
 package edu.uaslp.project;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RecordManager {
     private final int numMaxRecords;
-    private int countRecords;
     private final String nameFile;
 
     public RecordManager(int maxRecords, String name) {
-        numMaxRecords = maxRecords;
-        nameFile = name;
-        countRecords = 0;
+        this.numMaxRecords = maxRecords;
+        this.nameFile = name;
     }
 
     public void save(Record record) {
         if (record.getNamePlayer().equals(""))
             throw new RecordsException("Empty name not allowed");
 
-        countRecords++;
+        List<Record> records = getRecords();
 
-        if (countRecords >= numMaxRecords) {
-            this.saveInFullCase(record);
-        }
+        records.add(record);
+        records.sort((o1, o2) -> o2.getScorePlayer() - o1.getScorePlayer());
 
-        if (countRecords <= numMaxRecords) {
-            try (FileWriter fileWriter = new FileWriter(nameFile, true);
-                 PrintWriter printWriter = new PrintWriter(fileWriter)) {
-                printWriter.append(record.getNamePlayer()).append(" ").print(record.getScorePlayer() + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-    }
-
-    public void saveInFullCase(Record record) {
-        int i;
-        List<Record> auxRecords = this.getRecords();
-
-        countRecords = 0;
 
         try (FileWriter fileWriter = new FileWriter(nameFile);
              PrintWriter printWriter = new PrintWriter(fileWriter)) {
-            printWriter.write("");
+            int counter = 0;
+
+            for (Record record1 : records) {
+                printWriter.append(record1.getNamePlayer()).append(",").print(record1.getScorePlayer() + "\n");
+                counter++;
+                if (counter == numMaxRecords) {
+                    break;
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        auxRecords.remove(0);
 
-        this.save(record);
-        for (i = 0; i < numMaxRecords - 1; i++) {
-            this.save(auxRecords.get(i));
-        }
     }
 
     public List<Record> getRecords() {
-        List<Record> records = new ArrayList<Record>();
+        List<Record> records = new LinkedList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(nameFile))) {
             String currentLine;
 
             while ((currentLine = reader.readLine()) != null) {
-                String[] items = currentLine.split(" ");
+                String[] items = currentLine.split(",");
 
                 records.add(new Record(items[0], Integer.parseInt(items[1])));
             }
